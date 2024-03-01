acaf38d9fb649d6d9b8ce0346bbb80c4d3c0987b
sunyan-SG <45808179+sunyan-SG@users.noreply.github.com>
2024-03-01 19:52:25 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,150 @@
+from PyPDF2 import PdfReader
+#from langchain_community.embeddings import BedrockEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
+#from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+#from langchain.memory import ConversationMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
+from langchain.callbacks.base import BaseCallbackHandler
+#from langchain_community.llms import Bedrock
+#from langchain_community.chat_models import BedrockChat
+from langchain_openai import OpenAI, ChatOpenAI
+from langchain.schema import ChatMessage
+import streamlit as st
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+import os
+import sys
+import io
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+st.session_state.openai_key = os.getenv('OPENAI_API_KEY')
+
+st.set_page_config(page_title="AI Chatbot for custo",
+                   layout="wide",
+                   page_icon=":books::parrot:")
+
+#st.title("AI Chatbot for Customer Services")
+st.title("EMBA招生查询")
+
+# Create a container to display the chatbot's responses
+stream_handler = StreamHandler(st.empty())
+
+
+if "langchain_messages" not in st.session_state:
+    st.session_state["langchain_messages"] = []
+# Set up memory
+msgs = StreamlitChatMessageHistory(key="langchain_messages")
+
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("How can I help you today? 您好，请问有什么可以帮忙的？")
+
+st.sidebar.markdown(
+    """
+    ### Message from SungyBots:
+    Very welcome to try the chatbot developed by SungyBots 👏👏!!
+    
+    欢迎咨询我们的EMBA项目 👏👏！
+    
+    Any further queries, feel free contact us: contact@SungyBots.com
+    """
+)
+
+# retriever = None
+# st.session_state.vectorDB = None
+# st.session_state.pdf_docs = None
+
+# Define conversation_chain after vectorDB is set
+memory = ConversationBufferMemory(
+    memory_key="chat_history", input_key="question", return_messages=False)
+    #memory_key="chat_history",chat_memory=msgs,return_messages=True)
+
+
+current_directory = os.getcwd()
+path = current_directory+ '/' + 'vectorDB'
+#path = './' + 'vectorDB'
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={'device': 'cpu'})
+# embeddings = OpenAIEmbeddings()
+st.session_state.vectorDB = FAISS.load_local(path, embeddings)
+#st.success('Ready to start!', icon="✅")
+
+# # Debug: Print the status of OpenAI key and vectorDB
+# st.write("OpenAI key:", st.session_state.get("openai_key"))
+# st.write("VectorDB:", st.session_state.vectorDB)
+
+if st.session_state.vectorDB is not None:
+    vectorDB = st.session_state.vectorDB
+    if hasattr(vectorDB, 'as_retriever'):
+        # retriever: top 3 retrieved similarity search
+        retriever = vectorDB.as_retriever(
+            search_type="similarity",
+            search_kwargs={"k": 3})
+
+        llm = ChatOpenAI(
+            model_name="gpt-3.5-turbo",
+            # model_name="text-davinci-003",
+            openai_api_key=st.session_state.openai_key,
+            temperature=0,
+            streaming=True,
+            verbose=False,
+            #callbacks=[stream_handler],  ## this one must be removed
+        )
+
+        st.session_state.conversation_chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            chain_type="stuff",
+            # vectorstore=vectorDB,
+            retriever=retriever,
+            #verbose=False,
+            verbose = False,
+            memory=memory,
+        )
+
+# # Debug: Print the conversation_chain
+# st.write("conversation_chain:", st.session_state.conversation_chain)
+
+# Render current messages from StreamlitChatMessageHistory
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+prompt = st.chat_input("Your question here")
+if prompt:
+    if st.session_state.openai_key is None:
+        st.error("Please submit your OpenAI API key before using the chatbot.")
+    elif st.session_state.vectorDB is None:
+        st.error("Please upload PDF files and click 'Process' before using the chatbot.")
+    else:
+        response = st.session_state.conversation_chain.invoke(
+            {'question': prompt,
+             'chat_history': msgs.messages,
+             }
+        )
+        # qa = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff',
+        #                                  retriever=retriever)
+        # response= qa.invoke(prompt)
+        # ## above response type generated by RetrievalQA is different from conversation_chain
+
+        st.chat_message("user").write(prompt)
+        #st.chat_message("ai").write(response)
+        st.chat_message("ai").write(response["answer"])
+        msgs.add_user_message(prompt)
+        msgs.add_ai_message(response["answer"])
+
---
+++ file2embeddings.py
@@ -0,0 +1,85 @@
+import filetype  # For file type detection
+from docx import Document  # For Word file processing
+from PyPDF2 import PdfReader  # For PDF file processing
+import openpyxl
+
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import streamlit as st
+
+
+def get_file_text(uploaded_files):
+    text = ""
+    for file in uploaded_files:
+        kind = filetype.guess(file)  # Detect file type
+        if kind is None:
+            print(f'Cannot determine file type for: {file.name}')
+            continue
+
+        if kind.mime == 'application/pdf':
+            pdf_reader = PdfReader(file)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        elif kind.mime == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+            document = Document(file)
+            for paragraph in document.paragraphs:
+                text += paragraph.text
+        elif kind.mime == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+            workbook = openpyxl.load_workbook(file)
+            sheet = workbook.active  # Assuming you want the active sheet
+            for row in sheet.iter_rows():
+                row_text = ""  # Collect text for each row
+                for cell in row:
+                    if cell.value is not None:
+                        row_text += str(cell.value) + " "  # Adjust concatenation as needed
+                text += row_text + "\n"  # Add row to overall text
+        else:
+            print(f'Unsupported file type: {file.name}')
+
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", ".", " "],
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={'device': 'cpu'})
+    #embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+st.set_page_config(page_title="file uploader",
+                   layout="wide",
+                   page_icon=":books::parrot:")
+
+st.title("File Uploader for Sungy Customers")
+st.session_state.pdf_docs = st.file_uploader(
+    "Upload your PDFs here and click on 'Process'", type={"pdf", "docx", ".xlsx"}, accept_multiple_files=True)
+if st.button("Process"):
+    # with st.spinner("Processing"):
+    # get pdf text
+    raw_text = get_file_text(st.session_state.pdf_docs)
+    # get the text chunks
+    text_chunks = get_text_chunks(raw_text)
+    # create vector store
+    vectorDB = get_vectorstore(text_chunks)
+
+    # Save faiss vector to disk or store in a database/cloud storage
+    file = st.session_state.pdf_docs[0]
+    #st.write(file)
+    #name = 'vectorDB_'+file.name
+    name = 'vectorDB'
+    vectorDB.save_local(name)
+
+    st.success('PDF uploaded and embeddings created successfully!', icon="✅")
